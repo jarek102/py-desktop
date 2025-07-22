e70b4ff535f74b4930f4babdf54e5414e21d2297
Jarek Fuks <jarek102@gmail.com>
2025-07-22 11:34:31 +0200

    add versions

---
+++ src/versions.py
@@ -0,0 +1,11 @@
+import gi
+gi.require_version('Gtk', '4.0')
+gi.require_version('Astal','4.0')
+gi.require_version('AstalTray', '0.1')
+gi.require_version('AstalPowerProfiles', '0.1')
+gi.require_version('AstalMpris', '0.1')
+gi.require_version('AstalNetwork', '0.1')
+gi.require_version('AstalBattery', '0.1')
+gi.require_version('AstalHyprland', '0.1')
+gi.require_version('AstalBluetooth', '0.1')
+gi.require_version('AstalWp', '0.1')
