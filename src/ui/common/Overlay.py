a61b7a7d266ce6349cd17bbc01fa959c2c153282
Jarek102 <jarek102@gmail.com>
2026-02-03 01:08:19 +0100

    clean up blueprints

--- src/ui/common/Overlay.py
+++ src/ui/common/Overlay.py
@@ -1,19 +1,16 @@
 import gi
 from gi.repository import Astal, Gtk
+from utils import Blueprint
 
+@Blueprint("common/Overlay.blp")
 class Overlay(Astal.Window):
     __gtype_name__ = "Overlay"
 
     def __init__(self, on_close, **kwargs):
-        super().__init__(
-            anchor=Astal.WindowAnchor.TOP | Astal.WindowAnchor.BOTTOM | Astal.WindowAnchor.LEFT | Astal.WindowAnchor.RIGHT, # pyright: ignore[reportOperatorIssue]
-            layer=Astal.Layer.TOP,
-            exclusivity=Astal.Exclusivity.IGNORE,
-            keymode=Astal.Keymode.NONE,
-            css_classes=["overlay"],
-            **kwargs
-        )
-        
-        gesture = Gtk.GestureClick()
-        gesture.connect("pressed", lambda *_: on_close())
-        self.add_controller(gesture)
+        super().__init__(**kwargs)
+        self._on_close = on_close
+
+    @Gtk.Template.Callback()
+    def on_pressed(self, *args):
+        if self._on_close:
+            self._on_close()
--- src/ui/common/PopupWindow.py
+++ src/ui/common/PopupWindow.py
@@ -4,15 +4,8 @@ from gi.repository import Astal, Gtk
 class PopupWindow:
     """
     Mixin to configure a window as a popup.
-    Sets layer to OVERLAY and default anchors.
+    Sets layer to OVERLAY.
     Click-outside handling is managed by App overlays.
     """
     def setup_popup(self):
         self.set_layer(Astal.Layer.OVERLAY)
-        self.set_keymode(Astal.Keymode.ON_DEMAND)
-        self.set_exclusivity(Astal.Exclusivity.IGNORE)
-        
-        if self.get_anchor() == Astal.WindowAnchor.NONE:
-            self.set_anchor(Astal.WindowAnchor.TOP | Astal.WindowAnchor.RIGHT)
-            self.set_margin_top(4)
-            self.set_margin_right(4)
